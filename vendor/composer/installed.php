10f3fbdcdafd52d6c58c84b48c0a3ea67d2ace83
marikyle-rp <21001096@myrp.edu.sg>
2023-01-05 13:15:52 +0800

    commit message goes here

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor_name/heroku',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6d0016f72b7f2cd1fe3ac0f9eb8ff1e1e370dde4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/heroku' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6d0016f72b7f2cd1fe3ac0f9eb8ff1e1e370dde4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
